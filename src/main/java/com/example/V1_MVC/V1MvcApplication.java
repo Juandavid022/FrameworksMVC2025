919fadba0c0d34834ca562dd4cfc738bc4b071fb
Juandavid022 <58521859+Juandavid022@users.noreply.github.com>
2025-09-05 23:50:54 -0500

    cargue inicial

---
+++ src/main/java/com/example/V1_MVC/V1MvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.V1_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V1MvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/V1_MVC/V1MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.V1_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
