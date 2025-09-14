6aea0f2f92b303ed00c8b0d0d2191b44ffec18bb
Juandavid022 <58521859+Juandavid022@users.noreply.github.com>
2025-09-14 02:27:49 -0500

    entrega 2

---
+++ src/main/java/com/example/V1_MVC/Model/ClassModel.java
@@ -0,0 +1,39 @@
+package com.example.V1_MVC.Model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "class_tbl")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ClassModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String level; 
+
+    
+    @Column(nullable = false, unique = true)
+    private LocalDateTime schedule;
+
+    @ManyToOne
+    @JoinColumn(name = "coaches_id", referencedColumnName = "id")
+    private CoachModel coach;
+
+    }
